898dcfc6851ea0af8f76bae55d5dcd2994d38069
AndreyHein <soly.a@web.de>
2024-09-17 19:08:45 +0200

    lesson03

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-
+// LESSONS
+// 3 шаг - импортироуем компонент Lesson_02
 import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
-import Homework02 from "./homeworks/Homework02";
+import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
 
-import './App.css';
+// HOMEWORKS
+import Homework_02 from "./homeworks/Homework_02/Homework_02";
+
+// CONSULTATIONS
+
+import "./App.css";
+
+// Правила компонентов:
+// 1. Названия пишем с большой буквы
+// 2. Компонент - это функция и она должна возрващать JSX
+// 3. JSX - синтаксический сахар, позволяющий возращать HTML элементы из компонента, а также прописывать логику прямо в HTML
 
 function App() {
   return (
     <div className="App">
-    <Lesson_02 />
-    <Homework02 />
+      {/* Lessons */}
+      {/* 4 шаг - возвращаем компонент Lesson_02 из компонента App */}
+      {/* <Lesson_02 /> */}
+      <Lesson_03 />
+      {/* Homeworks */}
+      {/* <Homework_02 /> */}
     </div>
   );
 }
---
+++ src/homeworks/Homework_02/Homework_02.jsx
@@ -0,0 +1,55 @@
+import Card from "../../components/Card/Card";
+import "./styles.css";
+
+function Homework_02() {
+  const user1 = {
+    avatarUrl:
+      "https://whatsondisneyplus.b-cdn.net/wp-content/uploads/2021/09/homer.png",
+    firstName: "Homer",
+    lastName: "Simpson",
+    job: "Worker at Springfield Nuclear Power Plant",
+    hobbys: [
+      "Eating donuts ",
+      "watching TV ",
+      "sleeping ",
+      "spending time at Moe's Tavern ",
+    ],
+  };
+  const user2 = {
+    avatarUrl:
+      "https://whatsondisneyplus.b-cdn.net/wp-content/uploads/2021/09/marge.png",
+    firstName: "Marge",
+    lastName: "Simpson",
+    job: "Homemaker",
+    hobbys: [
+      "Cooking",
+      "Cleaning",
+      "Volunteering",
+      "Taking care of her family",
+    ],
+  };
+
+  const user3 = {
+    avatarUrl:
+      "https://whatsondisneyplus.b-cdn.net/wp-content/uploads/2021/09/bart-.png",
+    firstName: "Bart",
+    lastName: "Simpson",
+    job: "Student at Springfield Elementary",
+    hobbys: [
+      "Skateboarding",
+      "Playing pranks",
+      "Watching cartoons",
+      "Causing mischief",
+    ],
+  };
+
+  return (
+    <div className="homework02_wrapper">
+      <Card user={user1} />
+      <Card user={user2} />
+      <Card user={user3} />
+    </div>
+  );
+}
+
+export default Homework_02;
