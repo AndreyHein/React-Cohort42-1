3686ef08554f5d9327c7fb377b50af881fb53212
AndreyHein <soly.a@web.de>
2024-09-19 20:37:01 +0200

    consultation02

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@
 import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
 import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
 import Lesson_04 from "./lessons/Lesson_04/Lesson_04";
+import Consultation_02 from "./consultatons/Consultation_02/Consultation_02";
+
 
 // HOMEWORKS
 import Homework_02 from "./homeworks/Homework_02/Homework_02";
@@ -24,8 +26,9 @@ function App() {
       {/* 4 шаг - возвращаем компонент Lesson_02 из компонента App */}
       {/* <Lesson_02 /> */}
       {/* <Lesson_03 /> */}
-      <Lesson_04/>
+      {/* <Lesson_04/> */}
       {/* <Homework_03/> */}
+      <Consultation_02/>
       {/* Homeworks */}
       {/* <Homework_02 /> */}
     </div>
